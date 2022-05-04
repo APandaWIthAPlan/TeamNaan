6a5d4b708af66836f89a7291d234eb7f42eb81fe
APandaWithAPlan <nickythecreeper2@gmail.com>
2022-05-03 20:23:09 -0500

    as

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from Simon.Simon import simon
+import os
+import pygame
+from time import sleep
+
+title = pygame.image.load(os.path.join('MainAssets','bomb.png'))
+WHITE = (255,255,255)
+pygame.init()
+screen = pygame.display.set_mode((800,480))
+pygame.display.set_caption("Title")
+screen.fill(WHITE)
+screen.blit(title,(0,0))
+pygame.display.update()
+sleep(10)
 
 simon()
